80552b93b7a434fb503d279b87e5bc8397dfed92
thomas.bruneel <thomas.bruneel@hotmail.com>
2021-10-10 15:02:08 +0200

    add gson

---
+++ src/main/java/Config.java
@@ -0,0 +1,16 @@
+public class Config {
+
+    private String inputFolderPath;
+
+    public Config(String inputFolderPath) {
+        this.inputFolderPath = inputFolderPath;
+    }
+
+    public String getInputFolderPath() {
+        return inputFolderPath;
+    }
+
+    public void setInputFolderPath(String inputFolderPath) {
+        this.inputFolderPath = inputFolderPath;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,16 @@
+import com.google.gson.Gson;
 import org.apache.log4j.Logger;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 
 public class Main {
     static Logger myLogger = Logger.getLogger(Main.class.getName());
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
+        Config config = new Gson().fromJson(new FileReader("./config.json"), Config.class);
+        System.out.println(config.getInputFolderPath());
         myLogger.info("hello");
 
     }
