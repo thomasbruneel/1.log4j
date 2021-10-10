8db65090b28ae2268f64b7b2d6dbb398fbfb4fe7
thomas.bruneel <thomas.bruneel@hotmail.com>
2021-10-10 15:05:04 +0200

    changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,24 @@ import org.apache.log4j.Logger;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 
 public class Main {
     static Logger myLogger = Logger.getLogger(Main.class.getName());
 
-    public static void main(String[] args) throws FileNotFoundException {
-        Config config = new Gson().fromJson(new FileReader("./config.json"), Config.class);
-        System.out.println(config.getInputFolderPath());
+    public static void main(String[] args) throws IOException {
+        initInputFolder();
         myLogger.info("hello");
+
+    }
+
+    private static void initInputFolder() throws IOException {
+        Config config = new Gson().fromJson(new FileReader("./config.json"), Config.class);
+        Files.createDirectories(Paths.get(config.getInputFolderPath()));
+
 
     }
 }
